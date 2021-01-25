57554c849d9196b9998b4f26fd05527664342650
majkl zumberi <majklzumberi00@gmail.com>
2021-01-25 21:05:08 +0100

    style(module): :art: format module code style

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './customer/entities/customer.entity';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -21,6 +21,7 @@ import { ConfigModule } from '@nestjs/config';
       database: 'gestionale',
       entities: [Customer],
       synchronize: true,
+      keepConnectionAlive: true
     }),
   ],
   controllers: [AppController],
