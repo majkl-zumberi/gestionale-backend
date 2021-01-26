4291e8638e21247333a9a6424c9656db0250a3da
majkl zumberi <majklzumberi00@gmail.com>
2021-01-26 23:35:06 +0100

    fix(entity): :zap: add delete CASCADE on manyToOne relation for detail-order entity

--- src/detail-order/entities/detail-order.entity.ts
+++ src/detail-order/entities/detail-order.entity.ts
@@ -16,10 +16,14 @@ export class DetailOrder {
   @Column()
   quantity: number;
 
-  @ManyToOne(() => Article, (article: Article) => article.detailOrders)
+  @ManyToOne(() => Article, (article: Article) => article.detailOrders, {
+    onDelete: 'CASCADE',
+  })
   article: Article;
 
-  @ManyToOne(() => Order, (order: Order) => order.detailOrders)
+  @ManyToOne(() => Order, (order: Order) => order.detailOrders, {
+    onDelete: 'CASCADE',
+  })
   order: Order;
 
   // readonly totalprice
