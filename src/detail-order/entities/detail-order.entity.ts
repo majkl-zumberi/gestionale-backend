5b448bcd27143dc99c9d7573a0b90a1ee2eaa449
majkl zumberi <majklzumberi00@gmail.com>
2021-01-26 21:26:20 +0100

    feat(entity): :sparkles: add virtual total price for detail-order
    added afterLoad decorator to gain total price for article price times quantity

--- src/detail-order/entities/detail-order.entity.ts
+++ src/detail-order/entities/detail-order.entity.ts
@@ -1,18 +1,34 @@
-import { Article } from "src/article/entities/article.entity";
-import { Order } from "src/order/entities/order.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Article } from 'src/article/entities/article.entity';
+import { Order } from 'src/order/entities/order.entity';
+import {
+  AfterLoad,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class DetailOrder {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    quantity: number;
+  @Column()
+  quantity: number;
 
-    @ManyToOne(() => Article, (article: Article) => article.detailOrders)
-    article: Article;
+  @ManyToOne(() => Article, (article: Article) => article.detailOrders)
+  article: Article;
 
-    @ManyToOne(() => Order, (order: Order) => order.detailOrders)
-    order: Order;
+  @ManyToOne(() => Order, (order: Order) => order.detailOrders)
+  order: Order;
+
+  // readonly totalprice
+  protected total: number;
+
+  @AfterLoad()
+  getUrl = () => {
+    console.log({ qty: this.quantity });
+    console.log({ article: this.article });
+    this.total = this.quantity * Number(this.article.price);
+  };
 }
