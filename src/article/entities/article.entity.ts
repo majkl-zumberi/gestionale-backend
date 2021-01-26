b779423c1c0df4d021ef5dcaddc26cb18ed5b4c4
majkl zumberi <majklzumberi00@gmail.com>
2021-01-26 20:46:58 +0100

    fix(entity): :bug: price decimal type
    db now handle 2 numbers after comma

--- src/article/entities/article.entity.ts
+++ src/article/entities/article.entity.ts
@@ -2,33 +2,36 @@ import { type } from 'os';
 import { DetailOrder } from 'src/detail-order/entities/detail-order.entity';
 import { MeasureUnit } from 'src/measure-unit/entities/measure-unit.entity';
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    ManyToOne,
-    OneToMany,
-  } from 'typeorm';
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
 
 @Entity()
 export class Article {
-    @PrimaryGeneratedColumn()
-    id:number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column({type: 'decimal', precision:2})
-    price: number;
+  @Column({ type: 'decimal', precision: 4, scale: 2 })
+  price: number;
 
-    @Column({type:'date'})
-    expiry: Date;
+  @Column({ type: 'date' })
+  expiry: Date;
 
-    @Column()
-    available: number;
+  @Column()
+  available: number;
 
-    @ManyToOne(() => MeasureUnit, (measure: MeasureUnit) => measure.articles)
-    measure: MeasureUnit;
+  @ManyToOne(() => MeasureUnit, (measure: MeasureUnit) => measure.articles)
+  measure: MeasureUnit;
 
-    @OneToMany(() => DetailOrder, (detailOrder: DetailOrder) => detailOrder.article)
-    detailOrders: DetailOrder[];
+  @OneToMany(
+    () => DetailOrder,
+    (detailOrder: DetailOrder) => detailOrder.article,
+  )
+  detailOrders: DetailOrder[];
 }
