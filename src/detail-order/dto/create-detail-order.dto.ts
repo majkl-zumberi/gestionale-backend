fa9998a7dcc437b3c3e848044c6ee15277d87511
majkl zumberi <majklzumberi00@gmail.com>
2021-01-30 13:11:50 +0100

    refactor(dto): :zap: add more class validators for create detail order dto

--- src/detail-order/dto/create-detail-order.dto.ts
+++ src/detail-order/dto/create-detail-order.dto.ts
@@ -1,6 +1,13 @@
-import { IsNotEmpty } from "class-validator";
+import { IsInt, IsNotEmpty, Max, Min } from 'class-validator';
 
 export class CreateDetailOrderDto {
-    @IsNotEmpty()
-    quantity: number;
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1, {
+    message: 'la minima quantità deve essere almeno 1',
+  })
+  @Max(9000, {
+    message: 'la massima quantità è 9000',
+  })
+  quantity: number;
 }
