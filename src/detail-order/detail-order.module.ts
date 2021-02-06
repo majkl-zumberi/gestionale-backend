3e408d714395704f85bcc8dcad75c71f54dfb740
majkl zumberi <majklzumberi00@gmail.com>
2021-02-06 21:37:51 +0100

    style(module): :lipstick: refactor style

--- src/detail-order/detail-order.module.ts
+++ src/detail-order/detail-order.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DetailOrderService } from './detail-order.service';
-import { DetailOrderController } from './detail-order.controller';
-import { Article } from 'src/article/entities/article.entity';
-import { DetailOrder } from './entities/detail-order.entity';
-import { Order } from 'src/order/entities/order.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from 'src/article/entities/article.entity';
+import { Order } from 'src/order/entities/order.entity';
+import { DetailOrderController } from './detail-order.controller';
+import { DetailOrderService } from './detail-order.service';
+import { DetailOrder } from './entities/detail-order.entity';
 
 @Module({
   controllers: [DetailOrderController],
   providers: [DetailOrderService],
   imports: [TypeOrmModule.forFeature([Order, Article, DetailOrder])],
-  exports: [DetailOrderService]
+  exports: [DetailOrderService],
 })
 export class DetailOrderModule {}
