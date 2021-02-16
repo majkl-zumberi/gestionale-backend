a43ec9b221429a49752ee22584ed2bb34764ac6e
majkl zumberi <majklzumberi00@gmail.com>
2021-02-17 00:24:17 +0100

    fix(module): :sparkles: add Order in imports for invoice-tail.module

--- src/invoice-tail/invoice-tail.module.ts
+++ src/invoice-tail/invoice-tail.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { InvoiceTailService } from './invoice-tail.service';
-import { InvoiceTailController } from './invoice-tail.controller';
-import { InvoiceTail } from './entities/invoice-tail.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvoiceMaster } from 'src/invoice-master/entities/invoice-master.entity';
+import { Order } from 'src/order/entities/order.entity';
+import { InvoiceTail } from './entities/invoice-tail.entity';
+import { InvoiceTailController } from './invoice-tail.controller';
+import { InvoiceTailService } from './invoice-tail.service';
 
 @Module({
   controllers: [InvoiceTailController],
   providers: [InvoiceTailService],
-  imports: [TypeOrmModule.forFeature([InvoiceTail, InvoiceMaster])]
+  imports: [TypeOrmModule.forFeature([InvoiceTail, InvoiceMaster, Order])],
 })
 export class InvoiceTailModule {}
