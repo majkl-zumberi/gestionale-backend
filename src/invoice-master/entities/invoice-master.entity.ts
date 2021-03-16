8af2a169a776a1f969f493c5f272b293910a1534
majkl zumberi <majklzumberi00@gmail.com>
2021-03-16 18:34:32 +0100

    style(*): :art: format style improvement into invoice master entity

--- src/invoice-master/entities/invoice-master.entity.ts
+++ src/invoice-master/entities/invoice-master.entity.ts
@@ -1,40 +1,46 @@
-import { Customer } from "src/customer/entities/customer.entity";
-import { InvoiceTail } from "src/invoice-tail/entities/invoice-tail.entity";
-import { Invoice } from "src/invoice/entities/invoice.entity";
-import { Order } from "src/order/entities/order.entity";
-import { AfterLoad, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Customer } from 'src/customer/entities/customer.entity';
+import { InvoiceTail } from 'src/invoice-tail/entities/invoice-tail.entity';
+import { Invoice } from 'src/invoice/entities/invoice.entity';
+import { Order } from 'src/order/entities/order.entity';
+import {
+  AfterLoad,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class InvoiceMaster {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({ type: 'date' })
+  date: Date;
 
-    @Column({type:'date'})
-    date: Date;
+  @ManyToOne(() => Customer, (customer: Customer) => customer.masters, {
+    onDelete: 'CASCADE',
+  })
+  customer: Customer;
 
-    @ManyToOne(() => Customer, (customer: Customer) => customer.masters, {
-        onDelete: 'CASCADE',
-      })
-      customer: Customer;
-      
-    @ManyToOne(() => Order, (order: Order) => order.masters, {
-      onDelete: 'CASCADE',
-    })
-      order: Order;
-      
-      @OneToMany(() => Invoice, (invoice: Invoice) => invoice.master)
-      invoices: Invoice[];
+  @ManyToOne(() => Order, (order: Order) => order.masters, {
+    onDelete: 'CASCADE',
+  })
+  order: Order;
 
-      @OneToMany(() => InvoiceTail, (invoice: InvoiceTail) => invoice.master)
-      invoicesTail: InvoiceTail[];
+  @OneToMany(() => Invoice, (invoice: Invoice) => invoice.master)
+  invoices: Invoice[];
 
-    // readonly totalprice
+  @OneToMany(() => InvoiceTail, (invoice: InvoiceTail) => invoice.master)
+  invoicesTail: InvoiceTail[];
+
+  // readonly totalprice
   protected nDoc: string;
 
   @AfterLoad()
   calculateDocNumber = () => {
     //console.log(this.date, this.id);
-    this.nDoc = `${new Date(this.date).getFullYear()%100}/`+this.id;
+    this.nDoc = `${new Date(this.date).getFullYear() % 100}/` + this.id;
   };
 }
